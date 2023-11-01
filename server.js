632600b11f21bef4bb24baf672459f1fbdf9709c
Withor Lucas <withorcello@gmail.com>
2023-10-31 23:03:13 -0300

    teste de alteração

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mysql = require("mysql");
 const path = require("path");
 
-//configurando o ambiente
+//configurando o ambiente teste
 
 const app = express();
 
