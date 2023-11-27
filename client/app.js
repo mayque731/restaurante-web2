1a2d322ec40f7051e78f291cc0877e138dc85352
Thygasc <Mayque>
2023-11-27 08:30:47 -0400

    rotas concertadas entre as paginas

--- client/app.js
+++ client/app.js
@@ -6,8 +6,17 @@ const path = require("path");
 const app = express();
 
 app.use(express.json());
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./pages/mesas.html"));
+});
+
+app.get("/geladeira", (req, res) => {
+  res.sendFile(path.join(__dirname, "./pages/geladeira.html"));
+});
+
+app.get("/relatorios", (req, res) => {
+  res.sendFile(path.join(__dirname, "./pages/relatorios.html"));
 });
 
 app.listen(8080, () => {
